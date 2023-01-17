9b7bfdccb515ac4ac35ab4c276d146c39a691ef9
rohit0796 <91247476+rohit0796@users.noreply.github.com>
2023-01-17 10:42:14 +0530

    Update index.js

--- CURD/frontend/cudr/server/index.js
+++ CURD/frontend/cudr/server/index.js
@@ -15,7 +15,8 @@ mongoose.connect(datb, {
 app.use(express.json())
 app.use(cors())
 app.use('/', urlRouter)
-app.listen(5000, () => {
+const PORT= process.env.PORT|| 5000
+app.listen(PORT, () => {
     console.log("listening tp 5000")
 })  
 
